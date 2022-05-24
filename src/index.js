3ddb1451a7b2fd33921d9bfe5e7e08077eb89819
andisha <andon.stoichkov@yahoo.com>
2022-05-24 18:55:35 +0300

    Add logic for tab switching

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 import './style.css';
 import { initialPageLoad } from './page-load.js';
+import { homeContainer } from "./home-tab";
 
 initialPageLoad();
+
+const tabSwitcher = () => {
+    const navigation = document.querySelector('.navigation');
+    const tabs = navigation.querySelectorAll('li');
+    const content = document.getElementById('content');
+    let selectedTab = '';
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', (e) => {
+            selectedTab = e.target.id;
+            
+            switch (selectedTab) {
+                case 'home':
+                    content.innerHTML = '';
+                    content.appendChild(homeContainer());
+                    break;
+
+                default:
+                    break;
+            }
+
+        });
+    });
+
+
+
+};
+tabSwitcher();
