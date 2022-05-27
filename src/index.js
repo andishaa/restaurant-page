7e34a18e5009d828b438decfbccb888ce0fd8ad9
andisha <andon.stoichkov@yahoo.com>
2022-05-28 00:12:34 +0300

    Add the menu tab to switch tab logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import { initialPageLoad } from './page-load.js';
 import { homeContainer } from "./home-tab";
+import { menuContainer } from './menu-tab';
 
 initialPageLoad();
 
@@ -10,16 +11,21 @@ const tabSwitcher = () => {
     const content = document.getElementById('content');
     let selectedTab = '';
 
+    const clearContent = () => { content.innerHTML = '' };
+
     tabs.forEach(tab => {
         tab.addEventListener('click', (e) => {
             selectedTab = e.target.id;
-            
+
             switch (selectedTab) {
                 case 'home':
-                    content.innerHTML = '';
+                    clearContent();
                     content.appendChild(homeContainer());
                     break;
-
+                case 'menu':
+                    clearContent();
+                    content.appendChild(menuContainer());
+                    break;
                 default:
                     break;
             }
