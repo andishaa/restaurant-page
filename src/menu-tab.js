94dad69870a106bf0bd9245b0221e80f62c62497
andisha <andon.stoichkov@yahoo.com>
2022-05-28 00:00:56 +0300

    Create menu tab module and add dummy content

---
+++ src/menu-tab.js
@@ -0,0 +1,51 @@
+import ChefImg from './chefMenuImg.jpg';
+
+export const menuContainer = () => {
+    const container = document.createElement('div');
+    const chefMenu = document.createElement('div');
+    const wineMenu = document.createElement('div');
+    const aperetifsMenu = document.createElement('div');
+    const digestiveMenu = document.createElement('div');
+
+    chefMenu.classList.add('menu-item');
+    wineMenu.classList.add('menu-item');
+    aperetifsMenu.classList.add('menu-item');
+    digestiveMenu.classList.add('menu-item');
+    container.classList.add('menu-container');
+
+    const chefMenuTitle = document.createElement('div');
+    const chefMenuImg = new Image();
+    chefMenuImg.src = ChefImg;
+    chefMenuImg.classList.add('menu-img');
+    chefMenuTitle.classList.add('item-title');
+    chefMenuTitle.innerHTML = "Season of spring </br> Chef's tasting menu";
+    chefMenu.append(chefMenuImg, chefMenuTitle);
+
+    const wineMenuTitle = document.createElement('div');
+    const wineMenuImg = new Image();
+    wineMenuImg.src = ChefImg;
+    wineMenuImg.classList.add('menu-img');
+    wineMenuTitle.classList.add('item-title');
+    wineMenuTitle.innerHTML = "Season of spring </br> Chef's tasting menu";
+    wineMenu.append(wineMenuImg, wineMenuTitle);
+
+    const aperetifsMenuTitle = document.createElement('div');
+    const aperetifsMenuImg = new Image();
+    aperetifsMenuImg.src = ChefImg;
+    aperetifsMenuImg.classList.add('menu-img');
+    aperetifsMenuTitle.classList.add('item-title');
+    aperetifsMenuTitle.innerHTML = "Season of spring </br> Chef's tasting menu";
+    aperetifsMenu.append(aperetifsMenuImg, aperetifsMenuTitle);
+
+    const digestiveMenuTitle = document.createElement('div');
+    const digestiveMenuImg = new Image();
+    digestiveMenuImg.src = ChefImg;
+    digestiveMenuImg.classList.add('menu-img');
+    digestiveMenuTitle.classList.add('item-title');
+    digestiveMenuTitle.innerHTML = "Season of spring </br> Chef's tasting menu";
+    digestiveMenu.append(digestiveMenuImg, digestiveMenuTitle);
+
+    container.append(chefMenu, wineMenu, aperetifsMenu, digestiveMenu);
+
+    return container;
+}
